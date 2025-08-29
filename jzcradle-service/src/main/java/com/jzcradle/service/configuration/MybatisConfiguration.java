8707b72532e7cc2ac4b4cc8d4da42d036d54ae22
jiangzhe <827088212@qq.com>
2025-08-29 09:42:13 +0800

    init

---
+++ jzcradle-service/src/main/java/com/jzcradle/service/configuration/MybatisConfiguration.java
@@ -0,0 +1,23 @@
+package com.jzcradle.service.configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setDbType(DbType.POSTGRE_SQL);
+        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+}
